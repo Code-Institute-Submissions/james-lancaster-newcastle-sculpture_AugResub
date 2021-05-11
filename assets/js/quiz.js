47687b9a6d72cba5a0c3f594491debdcfb3809eb
james-lancaster <jameslancaster@outlook.com>
2021-05-11 15:50:22 +0000

    commence quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,4 @@
-document.getElementById("quizform").onsubmit=function({
+document.getElementById("quiz-form").onsubmit=function(){
        articulated = parseInt(document.querySelector('input[name = "articulated"]:checked').value),
 	   hume = parseInt(document.querySelector('input[name = "hume"]:checked').value),
 	   river = parseInt(document.querySelector('input[name = "river"]:checked').value),
@@ -9,22 +9,4 @@ document.getElementById("quizform").onsubmit=function({
 	document.getElementById("grade").innerHTML = result
 
 return false;
-});
-
-
-function sendMail(contactForm) {
-    emailjs.send("service_i2fp6ip", "contact_form_newcastlesc", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message_text": contactForm.messagetext.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILURE", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}
+};
