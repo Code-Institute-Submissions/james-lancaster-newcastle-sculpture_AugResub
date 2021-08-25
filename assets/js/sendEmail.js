ff58d814269ab4e684f2d6d182f3a37b8e1a6925
james-lancaster <jameslancaster@outlook.com>
2021-08-25 23:27:50 +0000

    minor changes to css

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,4 +1,8 @@
-function sendMail(contactForm) {
+const signUpForm = document.querySelector('#signUpForm')
+signUpForm.addEventListener("submit", event => {
+    event.preventDefault()
+    const contactForm = event.target;
+
     emailjs.send("service_i2fp6ip", "contact_form_newcastlesc", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
@@ -8,15 +12,11 @@ function sendMail(contactForm) {
         function(response) {
             console.log("SUCCESS", response);
             // Modal to acknowledge receipt of message
-            $(".acknowledgement-message").text("Thanks for being in touch " + contactForm.firstname.value);
+            $(".acknowledgement-message").text("Thanks for being in touch " + contactForm.name.value)
             $("#acknowledgementModal").modal("toggle");
-            $("#close-btn").click(function(){
-                location.reload();
-            });
         },
         function(error) {
             console.log("FAILED", error);
-        },
-    );
-    return false; // To block from loading a new page
-}
+        })
+        return false;  // To block from loading a new page
+});
