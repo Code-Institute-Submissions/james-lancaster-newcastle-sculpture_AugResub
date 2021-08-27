ca3fb74cd3d0df1006b338da718cc213e4c3c9c5
james-lancaster <jameslancaster@outlook.com>
2021-08-27 21:35:51 +0000

    add favicon

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,9 +1,5 @@
-//Run function only when the page is ready
-/* $(document).ready(function(){
-*/
-
 // When the user clicks on the "view image" button in question 4
-  $("#qzm01").click(function(){
+$("#qzm01").click(function(){
     $("#quizModal01").modal();
 /*    $('quizModal01').appendTo("body");*/
 });
@@ -22,33 +18,70 @@ $("#qzm04").click(function(){
 
 // Opens a modal to give a response when the user has selected an answer to any of the questions
 $(".btnQ1").click(function(){
-    if ($(this).hasClass('true')) {
-      $('#q1True').modal();
-    } else if ($(this).hasClass('false')) {
-      $('#q1False').modal();
+    if ($(this).hasClass('correct')) {
+        $('#q1Correct').modal();
+    } else if ($(this).hasClass('incorrect')) {
+        $('#q1Incorrect').modal();
+  }
+});
+
+$(".btnQ2").click(function(){
+    if ($(this).hasClass('correct')) {
+        $('#q2Correct').modal();
+    } else if ($(this).hasClass('incorrect')) {
+        $('#q2Incorrect').modal();
     }
+});
+
+$(".btnQ3").click(function(){
+    if ($(this).hasClass('correct')) {
+        $('#q3Correct').modal();
+    } else if ($(this).hasClass('incorrect')) {
+        $('#q3Incorrect').modal();
+    }
+});
+
+$(".btnQ4").click(function(){
+    if ($(this).hasClass('correct')) {
+        $('#q4Correct').modal();
+    } else if ($(this).hasClass('incorrect')) {
+        $('#q4Incorrect').modal();
+    }
+});
+
+// Changes background color depending on answer by adding appropriate class
+$('.sumQ').click(function() {
+   if ($(this).hasClass('correct')) {
+      $(this).addClass('right');
+   } else {
+      $(this).addClass('wrong');
+   }
   });
 
-  $(".btnQ2").click(function(){
-    if ($(this).hasClass('true')) {
-      $('#q2True').modal();
-    } else if ($(this).hasClass('false')) {
-      $('#q2False').modal();
-    }
-  });
+  //Fades out other quiz answers when one is selected 
+$('.btn-q').click(function(){
+  $(this).siblings().hide
+});
 
-  $(".btnQ3").click(function(){
-    if ($(this).hasClass('true')) {
-      $('#q3True').modal();
-    } else if ($(this).hasClass('false')) {
-      $('#q3False').modal();
-    }
-  });
+//Increases the score by one if the answer is correct
+var score = 0
+function check() {
+  var correct = 0;
+  $(".q1Sum").click(function(){
+      ($(this).hasClass('correct'))
+      correct++;
+  })
+  $(".q2Sum").click(function(){
+      ($(this).hasClass('correct'))
+      correct++;
+  })
+  $(".q3Sum").click(function(){
+      ($(this).hasClass('correct'))
+      correct++;
+  })
+  $(".q4Sum").click(function(){
+      ($(this).hasClass('correct'))
+      correct++;
+  })
+};
 
-  $(".btnQ4").click(function(){
-    if ($(this).hasClass('true')) {
-      $('#q4True').modal();
-    } else if ($(this).hasClass('false')) {
-      $('#q4False').modal();
-    }
-  });
