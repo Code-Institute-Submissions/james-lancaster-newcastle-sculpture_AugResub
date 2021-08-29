a8e3c2aba18c38c7c80ae8f8e5e3c74bb5c9fb4a
james-lancaster <jameslancaster@outlook.com>
2021-08-29 12:30:46 +0000

    development of map

---
+++ assets/js/locations.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 54.9783
+	        long: 1.6178
+        }
+    });
+}
